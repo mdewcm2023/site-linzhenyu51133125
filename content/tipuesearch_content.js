3cb9f5ff40c35df1e5e0090185c08d40c958ca9e
linzhenyu51133125 <linmuke123@gmail.com>
2023-03-31 11:14:24 +0800

    0.0

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'First', 'text': 'About\n cmsimde_site', 'tags': '', 'url': 'First.html'}]};
